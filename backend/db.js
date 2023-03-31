626a1e882ddbb72fb8072f9af6a8be5351155d70
ChetankrVyas <chetankrvyas@gmail.com>
2023-03-31 23:21:27 +0530

    doing

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://chetankrvyas:Chetan%402004@cluster0.4fri6ed.mongodb.net/praukcy"
-const connectToMongo = async () => {
- await mongoose.connect(mongoURI,() => {
-        console.log("connected to mongo successfully"); 
-    })
-}
-module.exports = connectToMongo;
